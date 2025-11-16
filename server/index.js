cd5a7231faff39a727ff08fab3dd1a38930d29af
Ashan-Edirisinghe <ashanedirisingheugc@gmail.com>
2025-11-17 02:19:45 +0530

    add: routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
+import postRoutes from './routes/posts.js';
 
 const app = express();
 app.use(bodyParser.json({limit: '30mb', extended: true }));
@@ -10,3 +11,10 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true }));
 app.use(cors());
 
 
+const CONNECTION_URL = 'mongodb+srv://AE_db_user:Johnwick4@cluster0.9027uj6.mongodb.net/';
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+app.use('/posts', postRoutes);
